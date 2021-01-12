939ddddd368ebd2d2721e9bb13124aeb483904ee
Jared Stromberg <jaredstromberg@gmail.com>
2021-01-12 07:54:43 -0700

    Built out tutorial example of accessing data via an independent backend dedicated to provide said data. There is no sample website that is used, but a filler is in its place and could be easily substittued. Of note, the @grafana/runtime package was not included in the boiler plate template for the plugin. This had to be added manually to package.json in order for the code to compile. Also, all remnants of the originally presented data source had to be commented out to eliminate multiple references to different things using the same terminology.

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -19,6 +19,15 @@ export class ConfigEditor extends PureComponent<Props, State> {
     onOptionsChange({ ...options, jsonData });
   };
 
+  onResolutionChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    const jsonData = {
+      ...options.jsonData,
+      resolution: parseFloat(event.target.value),
+    };
+    onOptionsChange({ ...options, jsonData });
+  };
+
   // Secure field (only sent to the backend)
   onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { onOptionsChange, options } = this.props;
@@ -60,6 +69,16 @@ export class ConfigEditor extends PureComponent<Props, State> {
             onChange={this.onPathChange}
             value={jsonData.path || ''}
             placeholder="json field returned to frontend"
+          />
+        </div>
+        <div className="gf-form">
+          <FormField
+            label="Resolution"
+            // labelWidth={6}
+            // inputWidth={20}
+            onChange={this.onResolutionChange}
+            value={jsonData.resolution || ''}
+            placeholder="Enter a number"
           />
         </div>
 
--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,4 +1,4 @@
-import defaults from 'lodash/defaults';
+// import defaults from 'lodash/defaults';
 
 import {
   DataQueryRequest,
@@ -9,57 +9,92 @@ import {
   FieldType,
 } from '@grafana/data';
 
-import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
+import { getBackendSrv } from '@grafana/runtime';
+
+import { MyQuery, MyDataSourceOptions /* defaultQuery */ } from './types';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  resolution: number;
+
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
     super(instanceSettings);
+
+    this.resolution = instanceSettings.jsonData.resolution || 1000.0;
+  }
+
+  async doRequest(query: MyQuery) {
+    const result = await getBackendSrv().datasourceRequest({
+      method: 'GET',
+      url: 'https://www.api.example.com/metrics',
+      params: query,
+    });
+    return result;
   }
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-    const { range } = options;
-    const from = range!.from.valueOf();
-    const to = range!.to.valueOf();
+    const promises = options.targets.map(query => {
+      this.doRequest(query).then(response => {
+        const frame = new MutableDataFrame({
+          refId: query.refId,
+          fields: [
+            { name: 'Time', type: FieldType.time },
+            { name: 'Value', type: FieldType.number },
+          ],
+        });
 
-    // Boiler plate query data built by yarn install-ing the template for a plugin provided by Grafana.
+        response.data.forEach((point: any) => {
+          frame.appendRow([point.time, point.value]);
+        });
 
-    // Return a constant for each query.
-    // const data = options.targets.map(target => {
-    //   const query = defaults(target, defaultQuery);
-    //   return new MutableDataFrame({
-    //     refId: query.refId,
-    //     fields: [
-    //       { name: 'Time', values: [from, to], type: FieldType.time },
-    //       { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
-    //     ],
-    //   });
-    // });
-
-    // Return a constant for each query.
-    const data = options.targets.map(target => {
-      const query = defaults(target, defaultQuery);
-      const frame = new MutableDataFrame({
-        refId: query.refId,
-        fields: [
-          { name: 'time', type: FieldType.time },
-          { name: 'value', type: FieldType.number },
-        ],
+        return frame;
       });
-
-      // duration of the time range, in milliseconds
-      const duration = to - from;
-
-      // step determines how close in time (ms) the points will be to each other
-      const step = duration / 1000;
-
-      for (let t = 0; t < duration; t += step) {
-        frame.add({ time: from + t, value: Math.sin((2 * Math.PI * query.frequency * t) / duration) });
-      }
-      return frame;
     });
-
-    return { data };
+    return Promise.all(promises).then(data => ({ data }));
   }
+
+  // const { range } = options;
+  // const from = range!.from.valueOf();
+  // const to = range!.to.valueOf();
+
+  // Boiler plate query data built by yarn install-ing the template for a plugin provided by Grafana.
+
+  // Return a constant for each query.
+  // const data = options.targets.map(target => {
+  //   const query = defaults(target, defaultQuery);
+  //   return new MutableDataFrame({
+  //     refId: query.refId,
+  //     fields: [
+  //       { name: 'Time', values: [from, to], type: FieldType.time },
+  //       { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
+  //     ],
+  //   });
+  // });
+
+  // Return a constant for each query.
+  //   const data = options.targets.map(target => {
+  //     const query = defaults(target, defaultQuery);
+  //     const frame = new MutableDataFrame({
+  //       refId: query.refId,
+  //       fields: [
+  //         { name: 'time', type: FieldType.time },
+  //         { name: 'value', type: FieldType.number },
+  //       ],
+  //     });
+
+  //     // duration of the time range, in milliseconds
+  //     const duration = to - from;
+
+  //     // step determines how close in time (ms) the points will be to each other
+  //     const step = duration / this.resolution;
+
+  //     for (let t = 0; t < duration; t += step) {
+  //       frame.add({ time: from + t, value: Math.sin((2 * Math.PI * query.frequency * t) / duration) });
+  //     }
+  //     return frame;
+  //   });
+
+  //   return { data };
+  // }
 
   async testDatasource() {
     // Implement a health check for your data source.
